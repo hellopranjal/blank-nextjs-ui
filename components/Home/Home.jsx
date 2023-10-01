9b214187153099f6b894721e02069f92782057b1
hellopranjal <hellopranjaldas@gmail.com>
2023-10-01 16:50:13 +0530

    components folder added

---
+++ components/Home/Home.jsx
@@ -0,0 +1,7 @@
+import styles from "./Home.module.scss";
+
+const Home = (props) => {
+  return <div>Home Page</div>;
+};
+
+export default Home;
---
+++ components/UI/Container/Container.jsx
@@ -0,0 +1,7 @@
+import styles from "./Container.module.scss";
+
+const Container = (props) => {
+  return <div>{props.children}</div>;
+};
+
+export default Container;
